723930306d1d76edf4507dd60ee6c1a65070b237
Sasa Schwartz and Steven Fleming <sasa@>
2020-01-27 15:39:08 -0800

    create checkbox on submit and strikethrough on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 function List() {
   this.tasks = [];
+  this.currentId = 0;
 };
 
 List.prototype.addTask = function(task) {
+  this.assignId(task);
   this.tasks.push(task);
 };
+
+List.prototype.assignId = function(task) {
+  this.currentId += 1;
+  task.id = this.currentId;
+}
 
 function Task(name) {
   this.name = name;
@@ -12,31 +19,46 @@ function Task(name) {
   this.subtasks = [];
 };
 
-Task.prototype.isComplete = function() {
-  this.completed = true;
+
+Task.prototype.changeComplete = function() {
+  this.completed = !this.completed;
 }
 
 Task.prototype.addSubtasks = function(subtask) {
   this.subtasks.push(subtask);
 }
 
-var laundry = new Task("laundry");
-laundry.isComplete();
-console.log(laundry);
+// var laundry = new Task("laundry");
+// laundry.isComplete();
+// console.log(laundry);
 
-laundry.addSubtasks("sheets");
-console.log(laundry);
-
-var list = new List();
-list.addTask("laundry");
-console.log(list);
+// laundry.addSubtasks("sheets");
+// console.log(laundry);
 
 $(document).ready(function(){
+  var list = new List();
   $("#toDoForm").submit(function(event) {
     event.preventDefault();
-    var item1 = $("#item1").val();
-    $("#output").text(item1);
+    var userInput = $("#item1").val();
+    var userTask = new Task(userInput);
+
+    list.addTask(userTask); 
+    $("#outputList").append(`<input type="checkbox" name="checkbox" id="${userTask.id}" class="checkbox" value=""><label for=${userTask.id}>${userTask.name}</label><br>`);
+
+    var userInput2 =$("#subTask1").val();
+    userTask.addSubtasks(userInput2);
+
+    $(`#${userTask.id}`).change(function() {
+      userTask.changeComplete();
+      if (userTask.completed) {
+        $(`label[for='${userTask.id}']`).addClass("strikethrough");
+        console.log(userTask.id)
+      } else {
+
+      }
+    });
+
   });
-  
+
 
 });
